c6b605da0ab0b67aa73a28186b00c189955f25de
AuriaZh <linjunz@uw.edu>
2025-06-03 05:54:52 -0700

    Delete doctor_portal directory

--- doctor_portal/src/config/firebaseConfig.ts
+++
@@ -1,5 +0,0 @@
-import { getFirestore } from 'firebase/firestore';
-import { storage } from '../firebase';
-import app from '../firebase';
-export const db = getFirestore(app);
-export { storage }; 
